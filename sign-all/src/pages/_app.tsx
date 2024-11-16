61d8631a20f275fd98db15a51e1213271ab5d078
AASani29 <alfey2001sani@gmail.com>
2024-11-16 15:37:06 +0600

    auth0 setup and configuration

--- sign-all/src/pages/_app.tsx
+++ sign-all/src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import "@/styles/globals.css"
 import type { AppProps } from "next/app"
+import {UserProvider} from "@auth0/nextjs-auth0/client";
 
 import { Toaster } from "react-hot-toast"
+import { User } from "lucide-react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <UserProvider>
       <Component {...pageProps} />
-      <Toaster />
-    </>
-  )
+    </UserProvider>
+  );
+
+  
 }
